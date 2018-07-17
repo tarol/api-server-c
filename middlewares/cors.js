fb20fb32a1c01af74917f9bae31b5e6dc665f7e2
江珊 <jiangshan@77ircloud.com>
2018-07-17 16:38:38 +0800

    fix: options的status改为204

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ module.exports = async function cors(ctx, next) {
   ctx.set('Access-Control-Allow-Credentials', true);
   ctx.set('Access-Control-Allow-Headers', '*');
   if (ctx.method === 'OPTIONS') {
-    ctx.status = 200;
+    ctx.status = 204;
   } else {
     await next();
   }
