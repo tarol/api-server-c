bbe8d7056f0b19f880405040ad84c6f170e6bb14
tarol <ctarol@163.com>
2022-01-25 12:22:38 +0800

    feat: 加入各种类型

--- ws/cp.js
+++ ws/cp.js
@@ -4,16 +4,52 @@
  * @Description:
  */
 
+const _ = require('lodash');
+
+let count = 0;
+
+function createAlarm() {
+  return {
+    type: 'add',
+    id: count++
+  };
+}
+
+function removeAlarm() {
+  return {
+    type: 'remove',
+    id: _.random(0, count)
+  };
+}
+
+function pack(type) {
+  const rst = [];
+  _.range(_.random(10, 100)).forEach(() => {
+    if(type === 'add') {
+      rst.push(createAlarm());
+    }
+    if(type === 'mixin') {
+      const number = _.random(0, 9);
+      if(number < 7) {
+        rst.push(createAlarm());
+      }else {
+        rst.push(removeAlarm());
+      }
+    }
+  });
+  return _.shuffle(rst);
+}
+
 process.on('message', msg => {
   if(msg === 'storm') {
-    const start = Date.now();
+    let start = Date.now();
     let now;
-    let count = 0;
-    while(now = Date.now(), now - start < 5000) {
-      process.send({
-        type: 'alarm',
-        id: count++
-      });
+    while(now = Date.now(), now - start < 2000) {
+      process.send(pack('add'));
+    }
+    start = now;
+    while(now = Date.now(), now - start < 3000) {
+      process.send(pack('mixin'));
     }
     console.log(count);
   }
